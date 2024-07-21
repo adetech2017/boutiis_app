bf2a8fbd3a3a0fef780a0976628c8314758b4961
adetech2017 <adeyemi879@gmail.com>
2024-07-21 22:33:18 +0100

    include files to message

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,7 +2,7 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from core.models import User
-from .models import PrivateRoom, Message
+from .models import FileModel, PrivateRoom, Message
 
 
 
@@ -27,6 +27,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             )
             await self.accept()
 
+
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
@@ -34,49 +35,71 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message_type = text_data_json.get('type')
         message = text_data_json.get('message')
+        file_content = text_data_json.get('file')
+        file_name = text_data_json.get('file_name')
+        file_type = text_data_json.get('file_type')
+        timestamp = text_data_json.get('timestamp')
 
         if self.scope.get('user') and not self.scope['user'].is_anonymous:
             user = self.scope['user'].username
 
             if message_type == 'typing':
-                # Broadcast typing status to room group
                 await self.channel_layer.group_send(
                     self.room_group_name,
                     {
                         'type': 'user_typing',
-                        'user': self.scope['user'].username,
+                        'user': user,
+                    }
+                )
+            elif message_type == 'file':
+                await self.save_file_to_db(file_content, file_name, file_type)
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'chat_file',
+                        'file': file_content,
+                        'file_name': file_name,
+                        'file_type': file_type,
+                        'user': user,
+                        'timestamp': timestamp,
                     }
                 )
             else:
-                # Save message to the database
                 await self.save_message_to_db(message)
-
-                # Broadcast message to room group
                 await self.channel_layer.group_send(
                     self.room_group_name,
                     {
                         'type': 'chat_message',
                         'message': message,
-                        'user': self.scope['user'].username,
+                        'user': user,
+                        'timestamp': timestamp,
                     }
                 )
-                print(f"Received message: {message}, from user: {user}")
+                print(f"Received message: {message}, from user: {user} at about: {timestamp}")
         else:
             print("Received message from anonymous or unauthenticated user.")
+
+
 
     async def chat_message(self, event):
         message = event['message']
         user = event['user']
+        timestamp = event['timestamp']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
+            'type': 'message',
             'message': message,
-            'user': user
+            'user': user,
+            'timestamp': timestamp,
         }))
+
+
 
     async def user_typing(self, event):
         user = event['user']
@@ -86,6 +109,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'type': 'typing',
             'user': user
         }))
+
+
+    async def chat_file(self, event):
+        file_content = event['file']
+        file_name = event['file_name']
+        file_type = event['file_type']
+        user = event['user']
+        timestamp = event['timestamp']
+
+        await self.send(text_data=json.dumps({
+            'type': 'file',
+            'file': file_content,
+            'file_name': file_name,
+            'file_type': file_type,
+            'user': user,
+            'timestamp': timestamp,
+        }))
+
 
     @database_sync_to_async
     def save_message_to_db(self, message):
@@ -97,3 +138,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             return
 
         return Message.objects.create(room=room, user=user, content=message)
+
+
+    @database_sync_to_async
+    def save_file_to_db(self, file_content, file_name, file_type):
+        room = PrivateRoom.objects.get(id=self.room_id)
+        user = User.objects.get(id=self.scope['user'].id)
+        return FileModel.objects.create(room=room, user=user, file=file_content, file_name=file_name, file_type=file_type)
--- chat/models.py
+++ chat/models.py
@@ -20,3 +20,15 @@ class Message(models.Model):
 
     def __str__(self):
         return f'{self.user.username}: {self.content}'
+
+
+class FileModel(models.Model):
+    room = models.ForeignKey(PrivateRoom, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='messages/files/')
+    file_name = models.CharField(max_length=255)
+    file_type = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: [File: {self.file_name}]'
